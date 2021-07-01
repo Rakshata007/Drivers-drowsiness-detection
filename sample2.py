5c508a4f783dc56ebd0efd5654c2174268efd079
Rakshata007 <63253581+Rakshata007@users.noreply.github.com>
2021-07-01 19:30:29 +0100

    Add files via upload

---
+++ sample2.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+#path = r'image.jpg'
+
+# src = cv2.imread(path)
+# window_name = 'Image'
+# image = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+# cv2.imshow(window_name, image)
+
+
+cap = cv2.VideoCapture('ex.mp4')
+
+while True:
+    #src = cv2.imread(path)
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
